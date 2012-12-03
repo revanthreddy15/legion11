a59d8ccc088af55e0024f7c54e469151ba30c228
revanth <revanthreddy15@yahoo.com>
2012-12-03 00:08:56 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Legion::Application.config.secret_token = '1650fec1260e25e0531d79adf1772c62b0e1a3d2c6512636627e1e62a18940fd5be23e642743fe8fd6ae9d4bbfc9308bd1bfc220230d276d7832aad975d9eae7'
